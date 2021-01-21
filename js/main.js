413bbafb6057a1b11efdeb219be1acdfb886d29e
dev14dh <dev14dh@gmail.com>
2021-01-21 02:57:12 -0800

    test

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const btn=document.getElementById('btn');
+const addform=document.getElementById('form');
+const container=document.getElementById('container');
+const heading=document.getElementById('heading');
+const phone=document.querySelector('.phone');
+const email=document.querySelector('.email');
+const city=document.querySelector('.city');
+const select=document.getElementById('select');
+const disclaimer=document.getElementById('disclaimer');
+const footer=document.getElementById('footer');
+
+addform.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const formData= new FormData(addform);
+    formData.append("add" ,1);
+
+    if(addform.checkValidity()===false){
+        e.preventDefault();
+        e.stopPropagation();
+        addform.classList.add("was-validated");
+        return false;
+    }
+
+}
+);
+
+
+btn.addEventListener('click' ,(e)=>{
+
+
+
+
+phone.style.marginTop="-15px";
+email.style.marginTop="-15px";
+city.style.marginTop="-15px";
+select.style.marginTop="-15px"
+footer.style.paddingBottom="0px";
+
+
+})
+
+
+
+
