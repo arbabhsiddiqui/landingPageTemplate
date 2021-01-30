965e27135b7f6847d01f8288c2f20521dfaf9596
Abdul Basit <Abdul.Basit@digihive.local>
2021-01-30 18:45:46 +0530

    update enquiry form

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,4 @@
-<script src="js/main.js"></script>
-    <!-- Optional JavaScript -->
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script
       src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
