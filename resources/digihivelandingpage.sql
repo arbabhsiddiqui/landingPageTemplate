36ac7c2a0342c2efaecc49912791f1be8ca31f8c
BasitRz <basitprince8001@gmail.com>
2021-01-30 21:03:05 +0530

    export database

--- resources/digihivelandingpage.sql
+++ resources/digihivelandingpage.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 27, 2021 at 01:09 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 30, 2021 at 03:31 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `landingpage`
+-- Database: `digihivelandingpage`
 --
 
 -- --------------------------------------------------------
@@ -27,14 +28,16 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL DEFAULT '0',
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -49,10 +52,12 @@ INSERT INTO `admin` (`id`, `name`, `email`, `password`, `role`, `created_at`) VA
 -- Table structure for table `dropdown1`
 --
 
-CREATE TABLE `dropdown1` (
-  `id` int(11) NOT NULL,
-  `value` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `dropdown1`;
+CREATE TABLE IF NOT EXISTS `dropdown1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `dropdown1`
@@ -71,18 +76,26 @@ INSERT INTO `dropdown1` (`id`, `value`) VALUES
 -- Table structure for table `dropdown2`
 --
 
-CREATE TABLE `dropdown2` (
-  `id` int(11) NOT NULL,
-  `value` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `dropdown2`;
+CREATE TABLE IF NOT EXISTS `dropdown2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `dropdown2`
 --
 
 INSERT INTO `dropdown2` (`id`, `value`) VALUES
-(17, 'sddf'),
-(18, 'sddf');
+(20, 'UX/UI Design and Development'),
+(21, 'SEO'),
+(22, 'Pay Per Click'),
+(23, 'Social Media Marketing'),
+(24, 'Design & Branding'),
+(25, 'Content & Strategy'),
+(26, 'Digital Events'),
+(27, 'Digital Productions');
 
 -- --------------------------------------------------------
 
@@ -90,10 +103,12 @@ INSERT INTO `dropdown2` (`id`, `value`) VALUES
 -- Table structure for table `dropdown3`
 --
 
-CREATE TABLE `dropdown3` (
-  `id` int(11) NOT NULL,
-  `value` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `dropdown3`;
+CREATE TABLE IF NOT EXISTS `dropdown3` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `dropdown3`
@@ -108,8 +123,9 @@ INSERT INTO `dropdown3` (`id`, `value`) VALUES
 -- Table structure for table `home_page`
 --
 
-CREATE TABLE `home_page` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `home_page`;
+CREATE TABLE IF NOT EXISTS `home_page` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `field1` varchar(255) NOT NULL,
   `field2` varchar(255) NOT NULL,
   `field3` varchar(255) NOT NULL,
@@ -125,7 +141,7 @@ CREATE TABLE `home_page` (
   `disclaimer_msg` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `sub_title` varchar(255) NOT NULL,
-  `date_of_updation` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_of_updation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `facebook_link` varchar(255) NOT NULL,
   `twitter_link` varchar(255) NOT NULL,
   `instagram_link` varchar(255) NOT NULL,
@@ -146,15 +162,16 @@ CREATE TABLE `home_page` (
   `border_color` varchar(255) NOT NULL,
   `btn_color` varchar(255) NOT NULL,
   `accent_color` varchar(255) NOT NULL,
-  `bg_color` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `bg_color` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `home_page`
 --
 
 INSERT INTO `home_page` (`id`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`, `dropdown1`, `dropdown2`, `dropdown3`, `bg_image`, `main_image`, `disclaimer_heading`, `disclaimer_msg`, `title`, `sub_title`, `date_of_updation`, `facebook_link`, `twitter_link`, `instagram_link`, `linkedin_link`, `youtube_link`, `whatsapp`, `seo_title`, `seo_keyword`, `seo_desc`, `og_title`, `og_desc`, `og_image`, `google_tag_manager`, `facebook_pixel`, `main_email`, `base_color`, `text_color`, `border_color`, `btn_color`, `accent_color`, `bg_color`) VALUES
-(1, 'workplace', '', '', '', '', '', 'city', '', '', '', 'Untitled-4.jpg', 'Disclaimer', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, beatae. Modi repudiandae accusamus ducimus culpa.', 'AyuSynk Festive Offer!', 'Fill The Below Form To Avail The limited Time Offer!', '2021-01-21 07:15:01', 'facebook link', 'Twitter Link', 'Instagram Link', 'LinkedIn Link', 'YouTube Link', '9305514423', 'seo title', 'seo keyword', 'seo description', 'og title', 'og description', '92449-full_pin-on-wallpapers.jpg', 'google tag manager', 'facebook pixel', 'dev13dh@gmail.com', 'dd', '#2C3693', 'fkkff', 'success', 'kjjjv', 'vv');
+(1, 'current city', 'Website URL', '', '', '', '', '', 'Services', '', 'background.jpg', 'LANDING PAGE WEBSITE (3).jpg', 'Disclaimer', 'By submitting the above form, you are agreeing to our terms and conditions', 'Is your business online yet', 'If not please provide your details below and we will make sure to bring you online', '2021-01-21 07:15:01', 'facebook link', 'Twitter Link', 'Instagram Link', 'LinkedIn Link', 'YouTube Link', '9305514423', 'seo title', 'seo keyword', 'seo description', 'og title', 'og description', 'bg.jpg', 'google tag manager', 'facebook pixel', 'dev13dh@gmail.com', 'dd', '#2C3693', 'fkkff', 'success', 'kjjjv', 'vv');
 
 -- --------------------------------------------------------
 
@@ -162,8 +179,9 @@ INSERT INTO `home_page` (`id`, `field1`, `field2`, `field3`, `field4`, `field5`,
 -- Table structure for table `page_enquiry`
 --
 
-CREATE TABLE `page_enquiry` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `page_enquiry`;
+CREATE TABLE IF NOT EXISTS `page_enquiry` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `phone_number` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -176,99 +194,16 @@ CREATE TABLE `page_enquiry` (
   `dropdown1_value` varchar(255) NOT NULL,
   `dropdown2_value` varchar(255) NOT NULL,
   `dropdown3_value` varchar(255) NOT NULL,
-  `status` int(11) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `page_enquiry`
 --
 
 INSERT INTO `page_enquiry` (`id`, `name`, `phone_number`, `email`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`, `dropdown1_value`, `dropdown2_value`, `dropdown3_value`, `status`) VALUES
-(1, 'Abdullah', '7398716634', 'basitprince8001@gmail.com', 'Lucknow', '', '', '', '', '', 'kanpur', '', '', 1),
-(2, 'Faheem', '7398716600', 'basit@gmail.com', '', '', '', '', '', '', 'lucknow', '', '', 1),
-(3, 'Abdul Basit', '7398716675', 'basitprince8001@gmail.com', 'Lucknow', '', '', '', '', '', 'mumbai', '', '', 1),
-(4, 'Abdullah', '7398716634', 'basitprince8001@gmail.com', '', '', '', '', '', '', 'lucknow', '', '', 1),
-(5, 'Basit', '7398716634', 'basit@gmail.com', '', '', '', '', '', '', 'lucknow', '', '', 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `dropdown1`
---
-ALTER TABLE `dropdown1`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `dropdown2`
---
-ALTER TABLE `dropdown2`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `dropdown3`
---
-ALTER TABLE `dropdown3`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `home_page`
---
-ALTER TABLE `home_page`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `page_enquiry`
---
-ALTER TABLE `page_enquiry`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `dropdown1`
---
-ALTER TABLE `dropdown1`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
-
---
--- AUTO_INCREMENT for table `dropdown2`
---
-ALTER TABLE `dropdown2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT for table `dropdown3`
---
-ALTER TABLE `dropdown3`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT for table `home_page`
---
-ALTER TABLE `home_page`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `page_enquiry`
---
-ALTER TABLE `page_enquiry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+(6, 'Basit', '9305514423', 'basitprince8001@gmail.com', 'Lucknow', '', '', '', '', '', '', 'Pay Per Click', '', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
